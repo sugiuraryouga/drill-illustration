fa671fc999cbb04f2876374a97ef035f6b267867
sugiuraryouga <61176802+sugiuraryouga@users.noreply.github.com>
2020-03-12 18:14:49 +0900

    hoge

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "categorys#index"
   resources :categorys, only: [:index, :new, :create,:show]
-  resources :posts, only: [:index, :new, :create,:show,:edit,:update]
+  resources :posts, only: [:index, :new, :create,:show,:edit,:update] do
+    collection do
+      get 'search'
+    end  
+  end
 end
