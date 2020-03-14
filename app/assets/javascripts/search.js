16f73d93cc5808716417bc3d1d20010155631e07
sugiuraryouga <61176802+sugiuraryouga@users.noreply.github.com>
2020-03-14 22:41:04 +0900

    Update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -11,11 +11,12 @@ $(function() {
     .done(function(posts) {
       if (posts.length !== 0) {
       
-        console.log(posts)
         const data = posts.map(obj => obj.title);
         console.log(data)
         $('#post_search').autocomplete({
-          source: data,
+          autoFocus: true,
+    　　　　source: data,
+    　　　　minLength: 1,
           
        
         });
